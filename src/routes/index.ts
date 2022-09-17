5312485411759d93047459a0b55ef387815d11a0
Sangjin <sangjin170202@naver.com>
2022-09-17 18:34:22 +0900

    update(routes): 로그인 및 Refresh Routes 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 
 import { createUser } from '../controller/user.controller';
+import { UserLogin, UserRefresh } from '../controller/auth.controller';
 
 const router = Router();
 
 // user
 router.post('/users', createUser);
+
+// auth
+router.post('/users/token', UserLogin);
+
+// refresh
+router.get('/users/token', UserRefresh);
 
 export default router;
