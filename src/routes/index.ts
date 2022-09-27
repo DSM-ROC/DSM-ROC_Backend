ba3202ec978e2fdbcfbf50dd69170d5a8bea19af
Sangjin <sangjin170202@naver.com>
2022-09-27 20:32:45 +0900

    update(routes): 기본적인 routes 설정

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,5 @@
 import { Router } from 'express';
 
-import { createUser } from '../controller/user.controller';
-import { loginValidation, refreshValidation } from '../validation/auth';
-import { UserLogin, UserLogout, UserRefresh } from '../controller/auth.controller';
-
 const router = Router();
-
-// user
-router.post('/users', createUser);
-
-// auth
-router.post('/users/token', loginValidation, UserLogin);
-router.delete('/users/token', refreshValidation, UserLogout);
-
-// refresh
-router.get('/users/token', UserRefresh);
 
 export default router;
