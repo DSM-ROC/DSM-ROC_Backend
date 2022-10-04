2ddf503ba179a9d11008a1cb3b5da1c98a46e908
Sangjin <sangjin170202@naver.com>
2022-10-04 20:46:02 +0900

    create(shared): exception 추가

---
+++ src/shared/exception.ts
@@ -0,0 +1,53 @@
+export class HttpError extends Error {
+  public statusCode: number;
+
+  public message: string;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
+
+export class UnAuthorizedError extends HttpError {
+  constructor() {
+    super(401, 'Unauthorized');
+  }
+}
+
+export class ConflictError extends HttpError {
+  constructor() {
+    super(409, 'Conflict');
+  }
+}
+
+export class ExpiredTokenError extends HttpError {
+  constructor() {
+    super(401, 'Expired Token');
+  }
+}
+
+export class ForbiddenError extends HttpError {
+  constructor() {
+    super(403, 'Forbidden Request');
+  }
+}
+
+export class NotFoundError extends HttpError {
+  constructor(url: string) {
+    super(404, `Not Found ${url}`);
+  }
+}
+
+export class BadRequestError extends HttpError {
+  constructor(message = 'Bad Request') {
+    super(400, message);
+  }
+}
+
+export class InternalServerError extends HttpError {
+  constructor() {
+    super(500, 'Interal Server Error');
+  }
+}
