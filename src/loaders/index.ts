9d74ebec626b88d2b5ed4686c21080a9e4cb7311
Sangjin <sangjin170202@naver.com>
2022-10-04 20:55:36 +0900

    create(loaders): application 실행 관련 loaders 추가

---
+++ src/loaders/index.ts
@@ -0,0 +1,9 @@
+import express, { Application } from 'express';
+import { connectDatabase } from './connectDatabase';
+import { loadExpress } from './express';
+
+export const initApplication = async () => {
+  await connectDatabase();
+  const app: Application = express();
+  loadExpress(app);
+};
