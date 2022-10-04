c0123846d6f5f032d2354e51252359a204a11115
Sangjin <sangjin170202@naver.com>
2022-10-04 21:01:24 +0900

    create(repositories): user repository logic 추가

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,34 @@
+import { EntityRepository, getCustomRepository, Repository } from 'typeorm';
+
+import { User } from '../entity/user';
+import { UserInfo } from '../shared/DataTransferObject';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  static getQueryRepository() {
+    return getCustomRepository(UserRepository);
+  }
+
+  async findUserById(id: number): Promise<User> {
+    const user = await this.findOne({ id });
+    return user;
+  }
+
+  async findUserByEmail(email: string): Promise<User> {
+    const user = await this.findOne({ email });
+    return user;
+  }
+
+  async createUser(userInfo: UserInfo): Promise<User> {
+    const newUser = this.create(userInfo);
+    const createUser = await this.save(newUser);
+    return createUser;
+  }
+
+  async findUserByIdentity(email: string): Promise<User> {
+    return this.createQueryBuilder('user')
+      .where('user.email = :email')
+      .setParameter('email', email)
+      .getOne();
+  }
+}
