5a7b56f829d1ab5bc2d1afb44b642fde9c0941f6
yebin <yebin5127@gmail.com>
2022-11-15 19:47:11 +0900

    update(controller): user api 수정

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,3 @@
-import { response } from 'express';
-import { access } from 'fs';
 import { verifyTokenOrDone } from '../middlewares/verifyTokenOrDone';
 import { UserRepository } from '../repositories/user.repository';
 import { UserService } from '../services/user.service';
@@ -19,14 +17,14 @@ export class UserController {
     const userInfoToCreate = req.body as UserInfo;
 
     const response: UserTokenResObj = await this.userService.createUser(userInfoToCreate);
-    res.status(201).json(response);
+    return res.status(201).json(response);
   };
 
   public login: BusinessLogic = async (req, res, next) => {
     const userInfoToLogin = req.body as UserLoginInfo;
 
     const response: UserTokenResObj = await this.userService.login(userInfoToLogin);
-    res.status(200).json(response);
+    return res.status(200).json(response);
   };
 
   public updateInfo: BusinessLogic = async(req, res, next) => {
@@ -34,7 +32,7 @@ export class UserController {
     const id = req.decoded.id;
 
     const response: UserUpdateInfo = await this.userService.updateUserInfo({ gender, nickname, id });
-    res.status(200).json(response);
+    return res.status(200).json(response);
   }
 
   public cancelMember: BusinessLogic = async(req, res, next) => {
@@ -51,11 +49,11 @@ export class UserController {
       req.decoded.sub,
       refreshToken.slice(7),
     );
-    res.status(200).json(response);
+    return res.status(200).json(response);
   };
 
   public showUserInfo: BusinessLogic = async (req, res, next) => {
     const response: UserInfoResObj = await this.userService.showUserInfo(req.params.id);
-    res.status(200).json(response);
+    return res.status(200).json(response);
   };
 }
