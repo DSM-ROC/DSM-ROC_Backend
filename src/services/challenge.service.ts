4a54251031f48ceb83df4e47d7ce025bb7f3216d
yebin <yebin5127@gmail.com>
2022-11-30 10:07:57 +0900

    update(service): challenge 가입 기능 logic 추가

--- src/services/challenge.service.ts
+++ src/services/challenge.service.ts
@@ -1,19 +1,32 @@
 import { Challenge } from "../entity/challenge";
 import { ChallengeRepository } from "../repositories/challenge.repository";
 import { User } from "../entity/user";
-import { ConflictError } from "../shared/exception";
+import { ConflictError, NotFoundError } from "../shared/exception";
 import { ChallengeInfo, UpdateInfo } from "../shared/DataTransferObject";
+import { JoinRepository } from "../repositories/join.repository";
 
 export class ChallengeService {
-    constructor(private challengeRepository: ChallengeRepository) {}
+    constructor(
+      private challengeRepository: ChallengeRepository,
+      private joinRepository: JoinRepository,
+    ) {}
 
     async createChallenge(challengeInfo : ChallengeInfo, user: User) {
         const alreadyChallenge = await this.challengeRepository.findByName(challengeInfo.name);
         if (alreadyChallenge) { throw new ConflictError(); };
         return await this.challengeRepository.createChallenge(challengeInfo, user);
-      }
+    }
     
     async searchChallenge(searchWord: string) {
       return await this.challengeRepository.searchChallenge(searchWord);
+    }
+
+    async joinChallenge(challengeId: number, user: User) {
+      const challenge = await this.challengeRepository.findById(challengeId);
+      if(challenge) {
+        if(!await this.joinRepository.checkChallenge(challengeId, user)) {
+          this.joinRepository.JoinChallenge(challengeId, user);
+        } else throw new ConflictError();
+      } else throw new NotFoundError('challenge');
     }
 }
