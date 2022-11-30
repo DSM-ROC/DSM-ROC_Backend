bcde4d919c2b08a763cda92be4c49bd33ccb6cb4
yebin <yebin5127@gmail.com>
2022-11-30 10:08:53 +0900

    update(controller): challenge 가입 기능 res,req 값 설정

--- src/controllers/challenge.controller.ts
+++ src/controllers/challenge.controller.ts
@@ -4,27 +4,40 @@ import { Challenge } from "../entity/challenge";
 import { BusinessLogic } from "../shared/BusinessLogicInterface";
 import { UserRepository } from "../repositories/user.repository";
 import { ChallengeInfo } from "../shared/DataTransferObject";
+import { JoinRepository } from "../repositories/join.repository";
 
 export class ChallengeController {
     private challengeService: ChallengeService = new ChallengeService(
-        ChallengeRepository.getQueryRepository()
+        ChallengeRepository.getQueryRepository(),
+        JoinRepository.getQueryRepository()
     );
 
     public createChallenge: BusinessLogic = async(req, res, next) => {
         const challengeInfo = req.body as ChallengeInfo;
         const user = req.decoded;
     
-        const newChallenge = await this.challengeService.createChallenge(challengeInfo, user);
+        const response = await this.challengeService.createChallenge(challengeInfo, user);
 
-        return res.status(201).json(newChallenge);
+        return res.status(201).json(response);
     };
 
     public searchChallenge: BusinessLogic = async(req, res, next) => {
         const searchWord = req.query.where as string;
         
-        const Challenge = await this.challengeService.searchChallenge(searchWord);
+        const response = await this.challengeService.searchChallenge(searchWord);
 
-        return res.status(200).json(Challenge);
+        return res.status(200).json(response);
+    }
+
+    public joinChallenge: BusinessLogic = async(req, res, next) => {
+        const challengeId = Number(req.params.challenge_id);
+        const user = req.decoded;
+
+        await this.challengeService.joinChallenge(challengeId, user);
+
+        return res.status(201).json({
+            message : 'joinChallenge success'
+        });
     }
 
 }
