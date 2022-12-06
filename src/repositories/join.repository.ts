22dbf27b283bba3bf57c4b5cdd0ead5e33d1e71c
yebin <yebin5127@gmail.com>
2022-12-06 19:26:33 +0900

    update(repository): join api 수정

--- src/repositories/join.repository.ts
+++ src/repositories/join.repository.ts
@@ -1,33 +1,33 @@
-import { EntityRepository, getCustomRepository, Repository } from "typeorm";
-import { Join } from "../entity/join";
-import { User } from "../entity/user";
+import { EntityRepository, getCustomRepository, Repository } from 'typeorm';
+import { Join } from '../entity/join';
+import { User } from '../entity/user';
 
 @EntityRepository(Join)
 export class JoinRepository extends Repository<Join> {
-    static getQueryRepository() {
-        return getCustomRepository(JoinRepository);
-    }
+  static getQueryRepository() {
+    return getCustomRepository(JoinRepository);
+  }
 
-    async JoinChallenge(challengeId: number, user: User) {
-       const join = new Join();
+  async JoinChallenge(challengeId: number, user: User) {
+    const join = new Join();
 
-       join.challengeId = challengeId;
-       join.userId = user.id;
+    join.challengeId = challengeId;
+    join.userId = user.id;
 
-       return await this.save(join);
-    }
+    return this.save(join);
+  }
 
-    async checkChallenge(challengeId: number, user: User) {
-        const challenge = await this.findOne({ challengeId, userId: user.id })
-        return challenge;
-    }
+  async checkChallenge(challengeId: number, user: User) {
+    const challenge = await this.findOne({ challengeId, userId: user.id });
+    return challenge;
+  }
 
-    async getChallengeMember(challengeId: number) {
-        return await this.createQueryBuilder('join')
-            .select('join.userId')
-            .addSelect('user.nickname')
-            .innerJoin('join.user', 'user')
-            .where('join.challengeId = :challenge_id', { challenge_id: challengeId })
-            .getMany();
-    }
+  async getChallengeMember(challengeId: number) {
+    return this.createQueryBuilder('join')
+      .select('join.userId')
+      .addSelect('user.nickname')
+      .innerJoin('join.user', 'user')
+      .where('join.challengeId = :challenge_id', { challenge_id: challengeId })
+      .getMany();
+  }
 }
