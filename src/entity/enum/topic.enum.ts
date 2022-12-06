4c57f1dbc2041cb9f9e92fd6fe89f57cb8f4b885
Sangjin <sangjin170202@naver.com>
2022-12-06 17:54:45 +0900

    update(enum): lint 실행에 따른 변경 사항

--- src/entity/enum/topic.enum.ts
+++ src/entity/enum/topic.enum.ts
@@ -1,12 +1,11 @@
 export enum Topic {
-    sport = '운동',
-    study = '스터디',
-    book = '독서',
-    art = '미술',
-    music = '음악',
-    job = '취업',
-    certificate = '자격증',
-    coding = '코딩',
-    etc = '기타'
+  sport = '운동',
+  study = '스터디',
+  book = '독서',
+  art = '미술',
+  music = '음악',
+  job = '취업',
+  certificate = '자격증',
+  coding = '코딩',
+  etc = '기타',
 }
-  
