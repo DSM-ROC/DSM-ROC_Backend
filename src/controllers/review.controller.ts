99701aba62131ed516c74dc73ca11fc1c3c1ad56
yebin <yebin5127@gmail.com>
2022-12-14 19:19:00 +0900

    update(controller): review 삭제 api 추가

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -25,10 +25,18 @@ export class ReviewController {
         const reviewId = Number(req.params.review_id);
         const user = req.decoded;
 
-        const response = await this.reviewService.updateReview(challengeId, reviewId, review, user);
+        await this.reviewService.updateReview(challengeId, reviewId, review, user);
 
         return res.status(200).json({ message : 'update success' });
     }
 
+    public deleteReview: BusinessLogic = async(req, res, next) => {
+        const challengeId = Number(req.params.challenge_id);
+        const reviewId = Number(req.params.review_id);
+        const user = req.decoded;
 
+        await this.reviewService.deleteReview(challengeId, reviewId, user);
+
+        return res.status(200).json({ message : 'delete success' });
+    }
 }
