fe3ebda24b06eade86d6fa0286147c9e53c01880
yebin <yebin5127@gmail.com>
2022-12-14 19:34:12 +0900

    update(controller): review 전체 보기 api 추가

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -1,42 +1,51 @@
-import { JoinRepository } from "../repositories/join.repository";
-import { ReviewRepository } from "../repositories/review.repository";
-import { ReviewService } from "../services/review.service";
-import { BusinessLogic } from "../shared/BusinessLogicInterface";
+import { JoinRepository } from '../repositories/join.repository';
+import { ReviewRepository } from '../repositories/review.repository';
+import { ReviewService } from '../services/review.service';
+import { BusinessLogic } from '../shared/BusinessLogicInterface';
 
-export class ReviewController { 
-    private reviewService: ReviewService = new ReviewService(
-        ReviewRepository.getQueryRepository(),
-        JoinRepository.getQueryRepository()
-    );
+export class ReviewController {
+	private reviewService: ReviewService = new ReviewService(
+		ReviewRepository.getQueryRepository(),
+		JoinRepository.getQueryRepository(),
+	);
 
-    public createReview: BusinessLogic = async(req, res, next) => {
-        const review = req.body.review;
-        const challengeId = Number(req.params.challenge_id);
-        const user = req.decoded;
+	public createReview: BusinessLogic = async (req, res, next) => {
+		const review = req.body.review;
+		const challengeId = Number(req.params.challenge_id);
+		const user = req.decoded;
 
-        const response = await this.reviewService.createReview(challengeId, review, user);
-        
-        return res.status(202).json(response);
-    }
+		const response = await this.reviewService.createReview(challengeId, review, user);
 
-    public updateReview: BusinessLogic = async(req, res, next) => {
-        const review = req.body.review;
-        const challengeId = Number(req.params.challenge_id);
-        const reviewId = Number(req.params.review_id);
-        const user = req.decoded;
+		return res.status(202).json(response);
+	};
 
-        await this.reviewService.updateReview(challengeId, reviewId, review, user);
+	public updateReview: BusinessLogic = async (req, res, next) => {
+		const review = req.body.review;
+		const challengeId = Number(req.params.challenge_id);
+		const reviewId = Number(req.params.review_id);
+		const user = req.decoded;
 
-        return res.status(200).json({ message : 'update success' });
-    }
+		await this.reviewService.updateReview(challengeId, reviewId, review, user);
 
-    public deleteReview: BusinessLogic = async(req, res, next) => {
-        const challengeId = Number(req.params.challenge_id);
-        const reviewId = Number(req.params.review_id);
-        const user = req.decoded;
+		return res.status(200).json({ message: 'update success' });
+	};
 
-        await this.reviewService.deleteReview(challengeId, reviewId, user);
+	public deleteReview: BusinessLogic = async (req, res, next) => {
+		const challengeId = Number(req.params.challenge_id);
+		const reviewId = Number(req.params.review_id);
+		const user = req.decoded;
 
-        return res.status(200).json({ message : 'delete success' });
-    }
+		await this.reviewService.deleteReview(challengeId, reviewId, user);
+
+		return res.status(200).json({ message: 'delete success' });
+	};
+
+	public getAllReview: BusinessLogic = async (req, res, next) => {
+		const challengeId = Number(req.params.challenge_id);
+		const user = req.decoded;
+
+		const response = await this.reviewService.getAllReview(challengeId, user);
+
+		return res.status(200).json(response);
+	};
 }
