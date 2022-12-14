a491964f61a17e84b14ea460effe7aa681e9e5df
yebin <yebin5127@gmail.com>
2022-12-14 17:12:47 +0900

    update(route): index route에 review 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { userServiceRouter } from './user.router';
 import { challengeServiceRouter } from './challenge.router';
+import { reviewServiceRouter } from './review.router';
 
 export const dsmlocRouter = () => {
   const app = Router();
 
   userServiceRouter(app);
+  reviewServiceRouter(app);
   challengeServiceRouter(app);
 
   return app;
