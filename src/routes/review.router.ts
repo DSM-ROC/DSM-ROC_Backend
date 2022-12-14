a632aa23325736fcba6ace0433b57c086488cfab
yebin <yebin5127@gmail.com>
2022-12-14 19:34:52 +0900

    update(route): review 전체 보기 url 설정

--- src/routes/review.router.ts
+++ src/routes/review.router.ts
@@ -1,16 +1,32 @@
-import { Router } from "express";
-import { ReviewController } from "../controllers/review.controller";
-import { errorHandler } from "../middlewares/errorHandler";
-import { verifyTokenMiddleware } from "../middlewares/verifyToken";
+import { Router } from 'express';
+import { ReviewController } from '../controllers/review.controller';
+import { errorHandler } from '../middlewares/errorHandler';
+import { verifyTokenMiddleware } from '../middlewares/verifyToken';
 
 const router: Router = Router();
-export const  reviewServiceRouter = (app: Router) => {
-    const reviewController: ReviewController = new ReviewController();
+export const reviewServiceRouter = (app: Router) => {
+	const reviewController: ReviewController = new ReviewController();
 
-    app.use('/challenge', router);
+	app.use('/challenge', router);
 
-    router.post('/:challenge_id/review', verifyTokenMiddleware, errorHandler(reviewController.createReview));
-    router.patch('/:challenge_id/review/:review_id', verifyTokenMiddleware, errorHandler(reviewController.updateReview));
-    router.delete('/:challenge_id/review/:review_id', verifyTokenMiddleware, errorHandler(reviewController.deleteReview));
-    
-}
+	router.post(
+		'/:challenge_id/review',
+		verifyTokenMiddleware,
+		errorHandler(reviewController.createReview),
+	);
+	router.patch(
+		'/:challenge_id/review/:review_id',
+		verifyTokenMiddleware,
+		errorHandler(reviewController.updateReview),
+	);
+	router.delete(
+		'/:challenge_id/review/:review_id',
+		verifyTokenMiddleware,
+		errorHandler(reviewController.deleteReview),
+	);
+	router.get(
+		'/:challenge_id/review',
+		verifyTokenMiddleware,
+		errorHandler(reviewController.getAllReview),
+	);
+};
