2bf7088008a56cd09769937f9dd4ab3bbe93702f
yebin <yebin5127@gmail.com>
2022-12-21 14:12:59 +0900

    update(service): 댓글 작성 기능 logic 추가

---
+++ src/services/comment.service.ts
@@ -0,0 +1,32 @@
+import { User } from '../entity/user';
+import { ChallengeRepository } from '../repositories/challenge.repository';
+import { CommentRepository } from '../repositories/comment.repository';
+import { JoinRepository } from '../repositories/join.repository';
+import { PostRepository } from '../repositories/post.repository';
+import { ForbiddenError, NotFoundError } from '../shared/exception';
+
+export class CommentService {
+	constructor(
+		private commentRepository: CommentRepository,
+		private challengeRepository: ChallengeRepository,
+		private joinRepository: JoinRepository,
+		private postRepository: PostRepository,
+	) {}
+
+	async createComment(challengeId: number, postId: number, text: string, user: User) {
+		await this.checkChallenge(challengeId, user);
+		await this.checkPost(challengeId, postId);
+
+		await this.commentRepository.createComment(postId, text, user);
+	}
+
+	async checkChallenge(challengeId: number, user: User) {
+		if (!(await this.challengeRepository.getOneChallenge(challengeId))) throw new NotFoundError();
+		if (!(await this.joinRepository.checkJoinChallenge(challengeId, user)))
+			throw new ForbiddenError();
+	}
+
+	async checkPost(challengeId: number, postId: number) {
+		if (!(await this.postRepository.checkPost(challengeId, postId))) throw new NotFoundError();
+	}
+}
