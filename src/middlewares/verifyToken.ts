0bbd01053b38b2096684b04e5973b990e8d17d26
Sangjin <sangjin170202@naver.com>
2022-12-27 11:13:21 +0900

    update(middlewares): verifyToken 에러 메시지 보기 편하게 수정

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -3,29 +3,29 @@ import { BusinessLogic } from '../shared/BusinessLogicInterface';
 import { errorHandler } from './errorHandler';
 import { TokenPayload } from '../shared/TokenPayloadInterface';
 import { config } from '../config';
-import { BadRequestError, ExpiredTokenError, UnAuthorizedError } from '../shared/exception';
+import { ExpiredTokenError, UnAuthorizedError } from '../shared/exception';
 
 const verifyTokenLogic: (type: string, headers: string) => BusinessLogic =
 	(type: string, headers: string) => (req, res, next) => {
 		try {
 			const token: string = req.headers[headers] as string;
 			if (!token) {
-				return next(new BadRequestError());
+				return next(new UnAuthorizedError('Token is not provided'));
 			}
 			const payload: TokenPayload = jwt.verify(
 				token.slice(7),
 				config.jwtSecret,
 			) as unknown as TokenPayload;
 			if (payload.type !== type) {
-				return next(new UnAuthorizedError());
+				return next(new UnAuthorizedError('Token type is not valid'));
 			}
 			req.decoded = payload;
 			next();
 		} catch (err) {
 			if (err.message === 'ExpiredError') {
-				next(new ExpiredTokenError());
+				next(new ExpiredTokenError('Token is expired'));
 			} else {
-				next(new UnAuthorizedError());
+				next(new UnAuthorizedError('Token is not valid'));
 			}
 		}
 	};
